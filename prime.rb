cf6142a4252633c7f33ccddbc5432ed8e5145f69
Matthew <matthew_mcaleer@hotmail.com>
2022-05-15 22:42:56 +1000

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        false
+    else
+        (2..number-1).to_a.all? do |number_to_divide|
+            number % number_to_divide != 0
+        end
+    end
+end
